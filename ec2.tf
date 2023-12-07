68977af92f383c37714c60fdc5848af3310cd812
Bdave237 <96090529+Bdave237@users.noreply.github.com>
2023-12-06 19:45:27 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -28,23 +28,7 @@ data "aws_ami" "RHEL" {
     # }
 }
 
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "172.16.0.0/16"
 
-  tags = {
-    Name = "tf-example"
-  }
-}
-
-resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-east-1a"
-
-  tags = {
-    Name = "tf-example"
-  }
-}
 
 
 
@@ -57,8 +41,6 @@ resource "aws_instance" "webec2" {
   ami                    = var.os_type ==  "WINDOWS"  ?  data.aws_ami.WINDOWS.id  :  data.aws_ami.RHEL.id
   instance_type          = var.instance_type
   # subnet_id              = aws_subnet.web_public_subnets[count.index].id
-  subnet_id              =  aws_subnet.my_subnet.id
-#   vpc_id = aws_vpc.my_vpc.id
 
 }
 
